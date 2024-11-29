cc995cc202b82aa842b67ff3f7de249b26ddf6ea
FebiMaharani <147477650+FebiMaharani@users.noreply.github.com>
2024-11-29 23:50:42 +0700

    Update OrderViewModel.kt

--- app/src/main/java/id/febimaharani/cupcakes/ui/OrderViewModel.kt
+++ app/src/main/java/id/febimaharani/cupcakes/ui/OrderViewModel.kt
@@ -11,93 +11,74 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
-/** Price for a single cupcake */
+// harga 1 cupcake
 private const val PRICE_PER_CUPCAKE = 2.00
 
-/** Additional cost for same day pickup of an order */
+// biaya tambahan pickup hari yang sama
 private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 
-/**
- * [OrderViewModel] holds information about a cupcake order in terms of quantity, flavor, and
- * pickup date. It also knows how to calculate the total price based on these order details.
- */
+// menyimpan informasi cupccake untuk jumlah, rasa dan pickup. dan hitung total harga berdasarkan detail pesanan.
 class OrderViewModel : ViewModel() {
 
-    /**
-     * Cupcake state for this order
-     */
-    private val _uiState = MutableStateFlow(OrderUiState(pickupOptions = pickupOptions()))
-    val uiState: StateFlow<OrderUiState> = _uiState.asStateFlow()
+    // status pesanan
+    private val _uiState = MutableStateFlow(OrderUiState(pickupOptions = pickupOptions())) // status UI dengan opsi pickup
+    val uiState: StateFlow<OrderUiState> = _uiState.asStateFlow() // akses ke status UI sebagai StateFlow
 
-    /**
-     * Set the quantity [numberCupcakes] of cupcakes for this order's state and update the price
-     */
+    // mengatur jumlah [numberCupcakes] cupcake untuk status pesanan dan update harga
     fun setQuantity(numberCupcakes: Int) {
-        _uiState.update { currentState ->
+        _uiState.update { currentState -> // update status UI
             currentState.copy(
-                quantity = numberCupcakes,
-                price = calculatePrice(quantity = numberCupcakes)
+                quantity = numberCupcakes, // update jumlah 
+                price = calculatePrice(quantity = numberCupcakes) // hitung dan update harga baru
             )
         }
     }
 
-    /**
-     * Set the [desiredFlavor] of cupcakes for this order's state.
-     * Only 1 flavor can be selected for the whole order.
-     */
+    // mengatur [desiredFlavor] untuk status pesanan hanya 1 rasa
     fun setFlavor(desiredFlavor: String) {
-        _uiState.update { currentState ->
-            currentState.copy(flavor = desiredFlavor)
+        _uiState.update { currentState -> // update status UI
+            currentState.copy(flavor = desiredFlavor) // Update flavor
         }
     }
 
-    /**
-     * Set the [pickupDate] for this order's state and update the price
-     */
+    // mengatur pickup time dan perbarui harga
     fun setDate(pickupDate: String) {
-        _uiState.update { currentState ->
+        _uiState.update { currentState -> // update status UI
             currentState.copy(
-                date = pickupDate,
-                price = calculatePrice(pickupDate = pickupDate)
+                date = pickupDate, // update tanggal pickup
+                price = calculatePrice(pickupDate = pickupDate) // hitung dan update harga berdasarkan tanggal
             )
         }
     }
 
-    /**
-     * Reset the order state
-     */
+    // perbarui status pesan
     fun resetOrder() {
-        _uiState.value = OrderUiState(pickupOptions = pickupOptions())
+        _uiState.value = OrderUiState(pickupOptions = pickupOptions()) 
     }
 
-    /**
-     * Returns the calculated price based on the order details.
-     */
+    // mengembalikan harga berdasarkan detail order
     private fun calculatePrice(
-        quantity: Int = _uiState.value.quantity,
-        pickupDate: String = _uiState.value.date
+        quantity: Int = _uiState.value.quantity, // ambil jumlah dari status UI 
+        pickupDate: String = _uiState.value.date // ambil tanggal dari status UI
     ): String {
-        var calculatedPrice = quantity * PRICE_PER_CUPCAKE
-        // If the user selected the first option (today) for pickup, add the surcharge
+        var calculatedPrice = quantity * PRICE_PER_CUPCAKE // hitung harga
+        // jika ambil hari ini
         if (pickupOptions()[0] == pickupDate) {
-            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP // tambahan biaya
         }
-        val formattedPrice = NumberFormat.getCurrencyInstance().format(calculatedPrice)
+        val formattedPrice = NumberFormat.getCurrencyInstance().format(calculatedPrice) // format harga jadi dolar
         return formattedPrice
     }
 
-    /**
-     * Returns a list of date options starting with the current date and the following 3 dates.
-     */
+    // mengembalikan daftar opsi ke 3 tanggal berikutnya
     private fun pickupOptions(): List<String> {
         val dateOptions = mutableListOf<String>()
-        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
-        val calendar = Calendar.getInstance()
-        // add current date and the following 3 dates.
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault()) // format tanggal yang diinginkan
+        val calendar = Calendar.getInstance() // kalender menambahkan 3 tanggal berikutnya
         repeat(4) {
-            dateOptions.add(formatter.format(calendar.time))
-            calendar.add(Calendar.DATE, 1)
+            dateOptions.add(formatter.format(calendar.time)) // tambah tanggal sekarang
+            calendar.add(Calendar.DATE, 1) // tambah 1 hari
         }
-        return dateOptions
+        return dateOptions // memngembalikan daftar opsi tanggal 
     }
 }
